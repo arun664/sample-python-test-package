f99b151f8a0b9189dcc4a3d5142ee31bd0b2ee2b
Arun Kumar M <ArunKumarManjunathaNayaka@my.unt.edu>
2024-09-12 12:00:59 -0500

    write sample distribution and package for our code

---
+++ setup.py
@@ -0,0 +1,23 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package needs
+    ],
+    test_suite='tests',
+    tests_require=[
+        'unittest2',  # For example, if you are using unittest2
+    ],
+    # Metadata
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of the package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_package',
+)
